b9c1ca48e375bea241451d71e8240f1d7acc7ee9
LuizFelixDev <luizhenriquefelix138@gmail.com>
2024-12-09 08:37:02 -0300

    Corrigindo bugs no módulo vendas

--- modulos/vendas/vendas.c
+++ modulos/vendas/vendas.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include "vendas.h"
 
 char tela_modulo_vendas(void){
     char op;
--- modulos/vendas/vendas.h
+++ modulos/vendas/vendas.h
@@ -1,5 +1,10 @@
+#ifndef VENDAS_H
+#define VENDAS_H
+
 char tela_modulo_vendas(void);
 void tela_cadastrar_venda(void);
 void tela_alterar_venda(void);
 void tela_excluir_venda(void);
 void tela_exibir_venda(void);
+
+#endif
